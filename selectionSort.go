0eea24139c52131c09876ddf97125b93851f1989
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2019-03-10 16:54:26 +0800

    add type students

--- selectionSort.go
+++ selectionSort.go
@@ -18,3 +18,21 @@ func SelectionSort(data sort.Interface) {
 		data.Swap(i, small)
 	}
 }
+
+type student struct {
+	ID    int
+	Name  string
+	Grade string
+}
+
+type students []*student
+
+func (s students) Len() int { return len(s) }
+
+func (s students) Less(i, j int) bool { return s[i].ID < s[j].ID }
+
+func (s students) Swap(i, j int) { s[i], s[j] = s[j], s[i] }
+
+func SortStudents(s students) {
+	SelectionSort(s)
+}
