6ea38a7ef47b39529c0af944b4328391bcaef501
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2019-03-10 21:45:02 +0800

    递归版本的选择排序 RecSelectionSort

--- selectionSort.go
+++ selectionSort.go
@@ -10,12 +10,38 @@ func SelectionSort(data sort.Interface) {
 	var i, j, small int
 	for i = 0; i < n-1; i++ {
 		small = i
+		// 循环不变式： small 是 A[i, k-1] 中最小元素的下标, k 是到达表达式 j < n 时 j 的值, k >= i+1
+		// 归纳依据: j 是 i + 1 时, 此时 small 是 i，所以 small 是 [i, i] 中最小元素的下标
+		// 归纳假设: small 已经是 A[i..k-1]中最小元素的下标
 		for j = i + 1; j < n; j++ {
 			if data.Less(j, small) {
 				small = j
 			}
 		}
 		data.Swap(i, small)
+	}
+}
+
+// RecSelectionSort 是递归版本的选择排序
+// 假设要排序的数据是在数组A[0..n-1]中。
+// (1) 从数组A的尾部，也就是从A[i..n-1]中，选出最小的元素。
+// (2) 将步骤(1)中选出的元素与A[i]互换。
+// (3) 将剩下的数组A[i+1..n-1]进行排序。
+func RecSelectionSort(data sort.Interface) {
+	recSelectionSort(data, 0)
+}
+
+func recSelectionSort(data sort.Interface, i int) {
+	n := data.Len()
+	if i < n {
+		var small = i
+		for j := i + 1; j < n; j++ {
+			if data.Less(j, small) {
+				small = j
+			}
+		}
+		data.Swap(i, small)
+		recSelectionSort(data, i+1)
 	}
 }
 
--- sort_test.go
+++ sort_test.go
@@ -5,16 +5,24 @@ import (
 	"testing"
 )
 
-func TestSelectionSort(t *testing.T) {
+func testSortFunc(t *testing.T, sortFunc func(sort.Interface)) {
 	tcs := []sort.Interface{
 		sort.IntSlice{3, 1, 4, 1, 5, 9, 2, 6, 5},
 		sort.StringSlice{"base", "ball", "mound", "bat", "glove", "batter"},
 	}
 
 	for _, tc := range tcs {
-		SelectionSort(tc)
+		sortFunc(tc)
 		if !sort.IsSorted(tc) {
 			t.Errorf("SelectionSort(%v) failed", tc)
 		}
 	}
+}
+
+func TestSelectionSort(t *testing.T) {
+	testSortFunc(t, SelectionSort)
+}
+
+func TestRecSelectionSort(t *testing.T) {
+	testSortFunc(t, RecSelectionSort)
 }
