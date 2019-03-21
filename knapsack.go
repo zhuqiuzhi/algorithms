5e726fec5779092ca293aafdb411b1429164643a
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2019-03-21 22:37:16 +0800

    solution for Knapsack Problem

---
+++ knapsack.go
@@ -0,0 +1,75 @@
+// Knapsack problem:
+// You have a knapsack to carry products for selling.
+// It holds up to a certain weight, not enough for carrying all your products-
+// you must choose which ones to carry.
+// Knowing the weight and sales value of each product, which choice of products gives the highest revenue?
+
+package algorithms
+
+type product struct {
+	Name   string
+	Weight float64
+	Value  float64
+}
+
+type products []product
+
+func (ps products) TotalWeight() float64 {
+	var total float64
+	for _, p := range ps {
+		total += p.Weight
+	}
+	return total
+}
+
+func (ps products) SalesValue() float64 {
+	var total float64
+	for _, p := range ps {
+		total += p.Value
+	}
+	return total
+}
+
+func Add(ps []products, e product) {
+	for i := range ps {
+		ps[i] = append(ps[i], e)
+	}
+}
+
+func BruteForceKnapsack(items []product, maxWeight float64) ([]product, float64) {
+	var result []product
+	bestValue := 0.0
+	for _, candidate := range PowerSet(items) {
+		got := candidate.SalesValue()
+		if candidate.TotalWeight() <= maxWeight && got > bestValue {
+			bestValue = got
+			result = candidate
+		}
+	}
+
+	return result, bestValue
+}
+
+// PowerSet returns power set of ps
+func PowerSet(items []product) []products {
+	var result []products
+
+	// add a empty set
+	result = append(result, products{})
+
+	// keeps track of the next product to consider
+	for _, e := range items {
+		// duplicate the result we already have
+		var copyResult = make([]products, len(result))
+		copy(copyResult, result)
+
+		// adding the current product to the duplicates
+		// Bug: for _, r := range result
+		for i := range result {
+			result[i] = append(result[i], e)
+		}
+		result = append(result, copyResult...)
+	}
+
+	return result
+}
---
+++ knapsack_test.go
@@ -0,0 +1,67 @@
+package algorithms
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestBruteForceKnapsack(t *testing.T) {
+	testKnapsack(t, BruteForceKnapsack)
+}
+
+//     10,[2,3,5,7],[1,5,2,4]
+func testKnapsack(t *testing.T, testFunc func(items []product, maxWeight float64) ([]product, float64)) {
+	tcs := []struct {
+		items     []product
+		maxWeight float64
+		want      []product
+		wantValue float64
+	}{
+		{
+			items: []product{
+				{"apple", 2, 1}, {"pen", 3, 5},
+				{"orange", 5, 2}, {"book", 7, 4},
+			},
+			maxWeight: 10,
+			want: []product{
+				{"pen", 3, 5}, {"book", 7, 4},
+			},
+			wantValue: 9,
+		},
+	}
+
+	for _, tc := range tcs {
+		got, bestValue := testFunc(tc.items, tc.maxWeight)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("BruteForceKnapsack(%v, %v) got product %v, want product %v", tc.items, tc.maxWeight, got, tc.want)
+		}
+		if bestValue != tc.wantValue {
+			t.Errorf("BruteForceKnapsack(%v, %v) got value %f, want value %f", tc.items, tc.maxWeight, bestValue, tc.wantValue)
+		}
+	}
+}
+
+func TestPowerSet(t *testing.T) {
+	testPowerSet(t, PowerSet)
+}
+
+func testPowerSet(t *testing.T, testFunc func(ps []product) []products) {
+	tcs := []struct {
+		in     []product
+		number int
+	}{
+		{
+			[]product{
+				{"apple", 2, 1}, {"pen", 3, 5},
+				{"orange", 5, 2}, {"book", 7, 4},
+			}, 16,
+		},
+	}
+	for _, tc := range tcs {
+		got := testFunc(tc.in)
+		if len(got) != tc.number {
+			t.Errorf("product set is %v, got the number of set in power set is %d, want %d", tc.in, len(got), tc.number)
+		}
+	}
+
+}
