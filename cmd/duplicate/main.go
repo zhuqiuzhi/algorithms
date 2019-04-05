bc3dc9826cf708efcb508971d2155b21ce5b71bc
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2019-04-05 21:21:13 +0800

    support os.Stdin

--- cmd/duplicate/main.go
+++ cmd/duplicate/main.go
@@ -7,10 +7,16 @@ import (
 
 // 面试同花顺的后端开发工程师的时候，有道笔试题是要求打印出文件中重复的行
 func main() {
-	inFile, err := os.Open(os.Args[1])
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "open %v: %v \n", os.Args[1], err)
-		os.Exit(1)
+	var inFile *os.File
+	if len(os.Args) > 1 {
+		var err error
+		inFile, err = os.Open(os.Args[1])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "open %v: %v \n", os.Args[1], err)
+			os.Exit(1)
+		}
+	} else {
+		inFile = os.Stdin
 	}
 
 	var lines = make(map[string]struct{})
