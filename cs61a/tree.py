2fa19cb6871cb7c759093aeb2f5b4d602ac92aac
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2023-11-18 20:19:15 +0800

    tree

---
+++ cs61a/tree.py
@@ -0,0 +1,14 @@
+class Tree:
+    def __init__(self, label, branches=[]) -> None:
+        self.label = label
+        for branch in branches:
+            assert(branch, isinstance(branch, Tree))
+        # create new list
+        self.branches = list(branches)
+
+def prun(t, n):
+    """Prun all subtres whose lable is n.
+    """
+    t.branches = [b for b in t.branches if b.label != n]
+    for branch in t.branches:
+        prun(branch, n)
