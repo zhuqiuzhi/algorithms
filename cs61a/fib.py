7eb1e5aa2f2550e1e75331b8c80370956a2d4e57
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2023-11-18 21:16:52 +0800

    memorized

---
+++ cs61a/fib.py
@@ -0,0 +1,23 @@
+def fib(n):
+    """Return n-st fibonacci sequence
+    """
+    if n == 0 or n == 1:
+        return n
+    else:
+        return fib(n-2) + fib(n-1)
+
+def count(f):
+    def counted(n):
+        # function also is class
+        counted.call_count += 1
+        return f(n)
+    counted.call_count = 0
+    return counted
+
+def memo(f):
+    cache = {}
+    def memorized(n):
+        if n not in cache:
+            cache[n] = f(n)
+        return cache[n]
+    return memorized
