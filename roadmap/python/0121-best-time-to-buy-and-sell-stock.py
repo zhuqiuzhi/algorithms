d1dc3d39c661d8318a8fa209e5b1a07947a65dc4
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2023-11-25 16:42:13 +0800

    best-time-to-buy-and-sell-stock

---
+++ roadmap/python/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def maxProfitBrute(self, prices: List[int]) -> int:
+        """ Best Time to Buy and Sell Stock 
+
+        >>> Solution().maxProfitBrute([7,1,5,3,6,4])
+        5
+        >>> Solution().maxProfitBrute([7,6,4,3,1])
+        0
+        """
+        maxProfit = 0
+        for start in range(0, len(prices)):
+            for end in range(start+1, len(prices)):
+                diff = prices[end] -prices[start]
+                if diff > maxProfit:
+                    maxProfit = diff
+    
+        return maxProfit 
+
+    def maxProfit(self, prices: List[int]) -> int:
+        """ Best Time to Buy and Sell Stock
+
+        >>> Solution().maxProfit([7,1,5,3,6,4])
+        5
+        >>> Solution().maxProfit([7,6,4,3,1])
+        0
+        """
+        result = 0
+        lowest = prices[0]
+        for price in prices:
+            if price < lowest:
+                lowest = price
+            else:
+                result = max(result, price - lowest)
+
+        return result
