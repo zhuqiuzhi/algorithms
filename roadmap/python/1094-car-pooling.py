675a4bdff5efce773c20cf1735042ab4fc7b0446
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2023-12-01 19:07:43 +0800

    fix

--- roadmap/python/1094-car-pooling.py
+++ roadmap/python/1094-car-pooling.py
@@ -7,6 +7,8 @@ class Solution:
     False
     >>> s.carPooling([[2,1,5],[3,3,7]], 5)
     True
+    >>> s.carPooling([[2,1,5],[3,5,7]], 3)
+    True
     """
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         diffs = [0 for i in range(0, 1001)] 
@@ -17,11 +19,12 @@ class Solution:
             begin = trip[1]
             end = trip[2] - 1
             diffs[begin] = diffs[begin] + numPassengersi
-            if end + 1 < capacity:
+            if end + 1 < len(diffs):
                 diffs[end+1] = diffs[end+1] - numPassengersi
 
         stations[0] = diffs[0]
         for i in range(1, 1001):
             stations[i] = stations[i-1] + diffs[i]
 
+        #print(stations)
         return not any([x > capacity for x in stations])
