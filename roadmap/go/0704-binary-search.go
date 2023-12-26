7e11dd333108be21e9fb48234bcf737d8e6b9f3d
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2023-12-26 17:17:07 +0800

    binary search

--- roadmap/go/0704-binary-search.go
+++ roadmap/go/0704-binary-search.go
@@ -3,6 +3,7 @@ package roadmap
 func search(nums []int, target int) int {
 	lo, hi := 0, len(nums)-1
 
+	// 循环不变式: a[lo, hi] 包含 target
 	for lo <= hi {
 		// mid := lo + (hi-lo)/2
 		mid := lo + (hi-lo)>>1
