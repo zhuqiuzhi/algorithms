b4f13a295de9da4f39e105f2a7c3997d524cdfce
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2024-01-09 22:10:33 +0800

    Climbing Stairs

---
+++ roadmap/go/1-DDynamicProgramming/70-climbing-stairs.go
@@ -0,0 +1,18 @@
+package roadmap
+
+// Leetcode Problem: https://leetcode.com/problems/climbing-stairs/
+func climbStairs(n int) int {
+	if n == 1 {
+		return 1
+	}
+	var dp = make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	// dp[3] = dp[2] + dp[1]
+	// dp[n] = dp[n-1] + dp[n-2]
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+
+	return dp[n]
+}
