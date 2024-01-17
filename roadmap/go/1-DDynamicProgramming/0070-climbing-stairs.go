01c1e2e433980b8d9a84612597980d67e1752da7
zhuqiuzhi <zhuqiuzhi91@gmail.com>
2024-01-17 15:49:41 +0800

    pre, prepre 节约空间

--- roadmap/go/1-DDynamicProgramming/0070-climbing-stairs.go
+++ roadmap/go/1-DDynamicProgramming/0070-climbing-stairs.go
@@ -1,6 +1,7 @@
 package roadmap
 
 // Leetcode Problem: https://leetcode.com/problems/climbing-stairs/
+// 空间复杂度是 O(n)
 func climbStairs(n int) int {
 	if n <= 2 {
 		return n
@@ -8,11 +9,30 @@ func climbStairs(n int) int {
 	var dp = make([]int, n+1)
 	dp[1] = 1
 	dp[2] = 2
-	// dp[3] = dp[2] + dp[1]
-	// dp[n] = dp[n-1] + dp[n-2]
 	for i := 3; i <= n; i++ {
 		dp[i] = dp[i-1] + dp[i-2]
 	}
 
 	return dp[n]
+}
+
+// 空间复杂度: O(1)
+// 节约空间, 使用两个变量 pre 和 prepre 记录 i-1 和 i-2
+func climbStairsO1(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	var ret int
+	prepre := 1
+	pre := 2
+	// 1, 2, 3, 5
+	for i := 3; i <= n; i++ {
+		ret = pre + prepre
+		// update
+		prepre = pre
+		pre = ret
+	}
+
+	return ret
 }
--- roadmap/go/1-DDynamicProgramming/0198-house-robber.go
+++ roadmap/go/1-DDynamicProgramming/0198-house-robber.go
@@ -1,7 +1,7 @@
 package roadmap
 
 // Leetcode Problem: https://leetcode.com/problems/house-robber/
-// 难点:
+// 难点: 当抢第 i 个房子时, 需要计算此时的最大价值
 func rob(nums []int) int {
 	if len(nums) < 1 {
 		return 0
@@ -17,4 +17,26 @@ func rob(nums []int) int {
 	}
 
 	return dp[len(nums)]
+}
+
+// 节约空间, 使用 pre 和 prepre 记录 i-1 和 i-2 的数据
+// 边界case: nums=[1]
+func robO1(nums []int) int {
+	if len(nums) < 1 {
+		return 0
+	}
+
+	var ret int
+	prepre, pre := 0, 0
+	for i := 0; i < len(nums); i++ {
+		// ret 是在第 i 个房子(i 从 0 开始)时做出能不触发警报能抢到最大价值
+		// 选择不抢, 则经过第 i -1 个房子的最大价值
+		// 选择抢, 则是经过第 i-2 个房子抢到的最大价值加上第 i 个房子的价值
+		ret = max(pre, prepre+nums[i])
+		// update
+		prepre = pre
+		pre = ret
+	}
+
+	return ret
 }
